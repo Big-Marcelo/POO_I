2bffdda10dabf3324797f2937998ba7fc23e7ed1
Marcelo <140878769+Big-Marcelo@users.noreply.github.com>
2025-03-10 21:51:19 -0300

    Add files via upload
    10/03 - Herança e Polimorfismo

---
+++ pessoa/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Pessoa
+{
+    public string nome;
+    public string rg;
+}
+public class Vendedor : Pessoa
+{
+    public double comissao;
+}
+class Program
+{
+    static void Main()
+    {
+    Vendedor vendedor = new Vendedor(); // Corrigido
+    vendedor.nome = "Maria";
+    vendedor.comissao = 1.1;
+
+    Pessoa pessoa = new Pessoa(); // Corrigido
+    pessoa.nome = "Joao";
+
+    Console.WriteLine(pessoa.nome);
+    Console.WriteLine(vendedor.nome);
+    Console.WriteLine(vendedor.comissao);
+
+    }   
+}
