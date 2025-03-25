a27377afa919d466d688982187bd03e41e64b380
Marcelo <140878769+Big-Marcelo@users.noreply.github.com>
2025-03-24 21:48:18 -0300

    Roteiro Aula 4

---
+++ RO_04_Ex1/Ex1/Program.cs
@@ -0,0 +1,46 @@
+﻿public abstract class Pagamento
+{
+    public abstract void ProcessarPagamento();
+}
+
+public class CartaoCredito : Pagamento
+{
+    public override void ProcessarPagamento()
+    {
+        Console.WriteLine("Processando pagamento com Cartão de Crédito.");
+    }
+}
+
+public class BoletoBancario : Pagamento
+{
+    public override void ProcessarPagamento()
+    {
+        Console.WriteLine("Processando pagamento com Boleto Bancário.");
+    }
+}
+
+public class Pix : Pagamento
+{
+    public override void ProcessarPagamento()
+    {
+        Console.WriteLine("Processando pagamento com Pix.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Pagamento> pagamentos = new List<Pagamento>
+        {
+            new CartaoCredito(),
+            new BoletoBancario(),
+            new Pix()
+        };
+
+        foreach (var pagamento in pagamentos)
+        {
+            pagamento.ProcessarPagamento();
+        }
+    }
+}
