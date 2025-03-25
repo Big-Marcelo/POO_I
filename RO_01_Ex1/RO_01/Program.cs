7b787495be831e95f6aa473609b50db745c9470a
Marcelo <140878769+Big-Marcelo@users.noreply.github.com>
2025-03-24 22:20:59 -0300

    Roteiro Aula 1 - Exercício 1
    01 - 
    a) Altere o exemplo acima adicionando um atributo cargo.
    b) Crie mais 3 objetos e atribua os seguintes cargos: Gerente, Desenvolvedor e
    Estagiário.
    c) Crie um novo método que informa o salário de cada um e imprima, o salário
    de cada cargo. Gerente:R$10.000,00, Desenvolvedor:R$5000,00 e
    Estagiário: R$ 100,00. Obs: O salário não deve ser um atributo.

---
+++ RO_01_Ex1/RO_01/Program.cs
@@ -0,0 +1,41 @@
+﻿public class Pessoa
+{
+    public string Nome, Cargo;
+    public int Idade;
+
+    public void Apresentar()
+    {
+        Console.WriteLine($"Olá, meu nome é {Nome}, tenho {Idade} anos e sou {Cargo}.");
+    }
+
+    public void InformarSalario()
+    {
+        int salario = Cargo switch
+        {
+            "Gerente" => 10000,
+            "Desenvolvedor" => 5000,
+            "Estagiário" => 1000,
+            _ => 0
+        };
+        Console.WriteLine($"Salário de {Cargo}: R${salario:N2}");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Pessoa p1 = new Pessoa { Nome = "João", Idade = 30, Cargo = "Gerente" };
+        Pessoa p2 = new Pessoa { Nome = "Maria", Idade = 25, Cargo = "Desenvolvedor" };
+        Pessoa p3 = new Pessoa { Nome = "José", Idade = 22, Cargo = "Estagiário" };
+        
+
+        List<Pessoa> pessoas = new List<Pessoa> { p1, p2, p3};
+
+        foreach (Pessoa pessoa in pessoas)
+        {
+            pessoa.Apresentar();
+            pessoa.InformarSalario();
+        }
+    }
+}
