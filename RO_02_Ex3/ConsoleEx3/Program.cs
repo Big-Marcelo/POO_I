57f924218ad56382f9857f272d872fc3c0df1adc
Marcelo <140878769+Big-Marcelo@users.noreply.github.com>
2025-03-30 15:57:35 -0300

    Roteiro Aula 2 - Exercício 3
    Exercício 3: Criando um Elevador
    Crie uma classe Elevador com os seguintes atributos privados:
    ● andarAtual (int)
    ● totalAndares (int)
    Use propriedades (get e set) para:
    ✔ O andarAtual não pode ser menor que 0 nem maior que totalAndares.
    ✔ Criar métodos Subir() e Descer() para mudar o andar atual.

---
+++ RO_02_Ex3/ConsoleEx3/Program.cs
@@ -0,0 +1,55 @@
+﻿class Elevador
+{
+    private int andarAtual;
+    private int totalAndares;
+
+    public Elevador(int totalAndares)
+    {
+        this.totalAndares = totalAndares;
+        this.andarAtual = 0; 
+    }
+
+    public int AndarAtual
+    {
+        get { return andarAtual; }
+        private set
+        {
+            if (value >= 0 && value <= totalAndares)
+            {
+                andarAtual = value;
+            }
+        }
+    }
+
+    public void Subir()
+    {
+        if (andarAtual < totalAndares)
+        {
+            andarAtual++;
+        }
+    }
+
+    public void Descer()
+    {
+        if (andarAtual > 0)
+        {
+            andarAtual--;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Elevador e = new Elevador(10); 
+        e.Subir();
+        e.Subir();
+        Console.WriteLine(e.AndarAtual); 
+        e.Descer();
+        Console.WriteLine(e.AndarAtual); 
+        e.Descer();
+        e.Descer(); 
+        Console.WriteLine(e.AndarAtual); 
+    }
+}
