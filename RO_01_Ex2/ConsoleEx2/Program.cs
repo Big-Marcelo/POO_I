a58ed86b61e8c0fa1b438a5222c4076aad3a94ab
Marcelo <140878769+Big-Marcelo@users.noreply.github.com>
2025-03-30 15:29:00 -0300

    Roteiro Aula 1 - Exercício 2
    02 - Sua equipe está reprogramando o jogo PAC-MAN e você ficou responsável por
    desenvolver a classe dos fantasminhas. Programe a classe com pelo menos
    seguintes atributos: habilidade, nick, cor. E seus métodos. GerarFantasma (basta
    imprimir os atributos) e o método Mover que deve receber o parâmetro em string
    para onde ele deve se mover e imprimir a mensagem “Nick” se moveu para
    “Parametro”.

---
+++ RO_01_Ex2/ConsoleEx2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace PacMan
+{
+    class Fantasma
+    {
+        public string Habilidade { get; set; }
+        public string Nick { get; set; }
+        public string Cor { get; set; }
+
+        public void GerarFantasma()
+        {
+            Console.WriteLine($"Fantasma gerado!");
+            Console.WriteLine($"Nick: {Nick}");
+            Console.WriteLine($"Cor: {Cor}");
+            Console.WriteLine($"Habilidade: {Habilidade}");
+        }
+
+        public void Mover(string direcao)
+        {
+            Console.WriteLine($"{Nick} se moveu para {direcao}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fantasma fantasma1 = new Fantasma
+            {
+                Nick = "Marcelo",
+                Cor = "Vermelho",
+                Habilidade = "Seguir o jogador"
+            };
+
+            fantasma1.GerarFantasma();
+
+            fantasma1.Mover("norte");
+        }
+    }
+}
