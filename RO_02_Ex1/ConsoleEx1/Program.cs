0e4e241398b7bf8b771c4dc3c5e7e5af59b0f68c
Marcelo <140878769+Big-Marcelo@users.noreply.github.com>
2025-03-30 15:37:28 -0300

    Roteiro Aula 2 - Exercício 1
    Exercício 1: Criando uma Classe Produto
    Crie uma classe Produto que tenha os seguintes atributos privados:
    ● nome (string)
    ● preco (decimal)
    Use propriedades (get e set) para:
    ✔ Permitir a leitura do nome.
    ✔ Garantir que o preço nunca seja negativo.
    Requisitos:
    1. Criar um construtor que recebe o nome e o preço do produto.
    2. Criar um método ExibirDetalhes() que imprime nome e preço.

---
+++ RO_02_Ex1/ConsoleEx1/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace ExercicioProduto
+{
+    class Produto
+    {
+        private string nome;
+        private decimal preco;
+
+        public string Nome
+        {
+            get { return nome; }
+        }
+
+        public decimal Preco
+        {
+            get { return preco; }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Erro: O preço não pode ser negativo.");
+                }
+                else
+                {
+                    preco = value;
+                }
+            }
+        }
+
+        public Produto(string nome, decimal preco)
+        {
+            this.nome = nome;
+
+            Preco = preco;
+        }
+
+        public void ExibirDetalhes()
+        {
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"Preço: R${Preco}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("Celular", 1500);
+            p.ExibirDetalhes();
+
+            p.Preco = -200;
+        }
+    }
+}
