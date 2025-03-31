1c6e89d48ef6de879601cb0b1856d84da2316e5f
Marcelo <140878769+Big-Marcelo@users.noreply.github.com>
2025-03-31 18:23:15 -0300

    Roteiro Aula 3 - Exercício 4
     Você deve criar um sistema de animais com duas interfaces: uma para voar e
    outra para nadar. Em seguida, implemente várias classes que representem animais
    que possuem essas habilidades.
    Objetivos:
    1. Definir uma interface chamada IVoar com o método Voar().
    2. Definir uma interface chamada INadar com o método Nadar().
    3. Criar classes para representar animais que implementam uma ou ambas as
    interfaces:
    ○ Pato (pode voar e nadar).
    ○ Águia (pode voar, mas não pode nadar).
    ○ Peixe (pode nadar, mas não pode voar).
    4. Criar uma classe Program que demonstre como essas classes interagem
    com as interfaces.

---
+++ RO_03_Ex4/ConsoleEx4/Program.cs
@@ -0,0 +1,58 @@
+﻿public interface IVoar
+{
+    void Voar();
+}
+
+public interface INadar
+{
+    void Nadar();
+}
+
+public class Pato : IVoar, INadar
+{
+    public void Voar()
+        {
+        Console.WriteLine("O pato está voando.");
+        }
+
+    public void Nadar()
+    {
+        Console.WriteLine("O pato está nadando.");
+    }
+}
+
+public class Aguia : IVoar
+{
+    public void Voar()
+    {
+        Console.WriteLine("A águia está voando.");
+    }
+}
+
+public class Peixe : INadar
+{
+    public void Nadar()
+    {
+        Console.WriteLine("O peixe está nadando.");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Pato pato = new Pato();
+        Aguia aguia = new Aguia();
+        Peixe peixe = new Peixe();
+
+        Console.WriteLine("Ações do Pato:");
+        pato.Voar();
+        pato.Nadar();
+
+        Console.WriteLine("\nAções da Águia:");
+        aguia.Voar();
+
+        Console.WriteLine("\nAções do Peixe:");
+        peixe.Nadar();
+    }
+}
