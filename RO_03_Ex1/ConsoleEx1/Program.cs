e78aa3eea1025c5c4cbaa65e9304b23069618391
Marcelo <140878769+Big-Marcelo@users.noreply.github.com>
2025-03-31 16:40:24 -0300

    Roteiro Aula 3 - Exercício 1
    01 - Crie uma classe Animal com o atributo nome e um método EmitirSom() que
    printa o texto “Som genérico do animal”. Em seguida crie uma outra classe que
    herde a classe Animal e “emita” seu respectivo som.

---
+++ RO_03_Ex1/ConsoleEx1/Program.cs
@@ -0,0 +1,36 @@
+﻿class Animal
+{
+    public string Nome { get; set; }
+
+    public Animal(string nome)
+    {
+        Nome = nome;
+    }
+
+    public virtual void EmitirSom()
+    {
+        Console.WriteLine("Som genérico do animal");
+    }
+}
+
+class Cachorro : Animal
+{
+    public Cachorro(string nome) : base(nome) { }
+
+    public override void EmitirSom()
+    {
+        Console.WriteLine("Au Au!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal animal = new Animal("Gato");
+        animal.EmitirSom();
+
+        Cachorro cachorro = new Cachorro("Gato");
+        cachorro.EmitirSom();
+    }
+}
