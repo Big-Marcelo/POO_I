3b866aaaf36ec532cc54cca393bbcd0c95f8ea55
Marcelo <140878769+Big-Marcelo@users.noreply.github.com>
2025-03-31 17:59:13 -0300

    Roteiro Aula 3 - Exercício 3
    3 - Crie uma interface chamada IVeiculo com um método chamado Mover(). Em
    seguida, crie duas classes que implementem essa interface:
    1. Carro: A classe Carro deve implementar o método Mover() e exibir a
    mensagem "O carro está se movendo".
    2. Bicicleta: A classe Bicicleta também deve implementar o método
    Mover() e exibir a mensagem "A bicicleta está se movendo".
    No método Main, crie uma instância de cada classe e chame o método Mover() de
    ambas.

---
+++ RO_03_Ex3/ConsoleEx3/Program.cs
@@ -0,0 +1,32 @@
+﻿public interface IVeiculo
+{
+    void Mover();
+}
+
+public class Carro : IVeiculo
+{
+    public void Mover()
+    {
+        Console.WriteLine("O carro está se movendo");
+    }
+}
+
+public class Bicicleta : IVeiculo
+{
+    public void Mover()
+    {
+        Console.WriteLine("A bicicleta está se movendo");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IVeiculo carro = new Carro();
+        IVeiculo bicicleta = new Bicicleta();
+
+        carro.Mover();
+        bicicleta.Mover();
+    }
+}
