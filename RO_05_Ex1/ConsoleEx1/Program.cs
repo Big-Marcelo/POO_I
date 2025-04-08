51fc2112708da399f80a8179c64bcac4b38ccbc4
Marcelo <140878769+Big-Marcelo@users.noreply.github.com>
2025-04-07 22:00:21 -0300

    Roteiro Aula 5 - Exercício 1
    Crie um programa em C# que:
    1. Peça ao usuário que digite 5 números decimais.
    2. Para cada número digitado, exiba:
    ○ O número original.
    ○ O valor com arredondamento padrão (Math.Round).
    ○ O valor com arredondamento para baixo (Math.Floor).
    ○ O valor com arredondamento para cima (Math.Ceiling).
    ○ O valor com truncamento (Math.Truncate).

---
+++ RO_05_Ex1/ConsoleEx1/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main()
+    {
+        double[] numeros = new double[5];
+
+        Console.WriteLine("Digite 5 números decimais: ");
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write($"Número {i + 1}: ");
+            while (!double.TryParse(Console.ReadLine(), out numeros[i]))
+            {
+                Console.Write("Entrada inválida. Digite um número decimal válido: ");
+            }
+        }
+
+        Console.WriteLine("\nResultados: ");
+        for (int i = 0; i < 5; i++)
+        {
+            double numero = numeros[i];
+            Console.WriteLine($"\nNúmero {i + 1}: {numero}");
+            Console.WriteLine($"Arredondando (Math.Round): {Math.Round(numero)}");
+            Console.WriteLine($"Arredondando para baixo (Math.Floor): {Math.Floor(numero)}");
+            Console.WriteLine($"Arredondando para cima (Math.Ceiling): {Math.Ceiling(numero)}");
+            Console.WriteLine($"Truncado (Math.Truncade: {Math.Truncate(numero)})");
+
+        }
+    }
+}
