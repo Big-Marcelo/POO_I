c8211698c0edee90907a7c1188ff5a8395b7f5f2
Marcelo <140878769+Big-Marcelo@users.noreply.github.com>
2025-05-20 15:23:27 -0300

    Roteiro Aula 7 - Exercício 3

---
+++ RO_07_Ex3/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        HashSet<int> numerosUnicos = new HashSet<int>();
+        int numero;
+
+        Console.WriteLine("Digite números inteiros (digite 0 para parar):");
+
+        do
+        {
+            Console.Write("Digite um número: ");
+            string entrada = Console.ReadLine();
+
+            if (int.TryParse(entrada, out numero))
+            {
+                if (numero != 0)
+                {
+                    numerosUnicos.Add(numero);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Por favor, digite um número inteiro válido.");
+            }
+
+        } while (numero != 0);
+
+        Console.WriteLine("\nNúmeros únicos digitados:");
+        foreach (int n in numerosUnicos)
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
