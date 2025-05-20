feb17c08f6c696a1b425b837cf68ca086c1c51af
Marcelo <140878769+Big-Marcelo@users.noreply.github.com>
2025-05-20 00:45:09 -0300

    Roteiro Aula 6 - Exercício 4

---
+++ RO_06_Ex4/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class IdadeInvalidaException : Exception
+{
+    public IdadeInvalidaException() 
+        : base("Erro: Idade mínima para acesso é 18 anos.") 
+    {
+    }
+}
+
+class Program
+{
+    static void VerificarIdade(int idade)
+    {
+        if (idade < 18)
+        {
+            throw new IdadeInvalidaException();
+        }
+        else
+        {
+            Console.WriteLine("Acesso permitido.");
+        }
+    }
+
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Digite a idade: ");
+            int idade = int.Parse(Console.ReadLine());
+            VerificarIdade(idade);
+        }
+        catch (IdadeInvalidaException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Erro: Digite um número válido.");
+        }
+    }
+}
